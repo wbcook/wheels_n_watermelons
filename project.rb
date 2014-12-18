ba2416e75077d0232cfea58d117400d97b468de3
William Cook <wbcook@users.noreply.github.com>
2014-12-18 17:47:24 -0500

    Create project.rb

---
+++ project.rb
@@ -0,0 +1,26 @@
+# A fundraising app that tracks the funding of various odd projects. Now with encapsulation
+# and a Fundraising class to manage lists of proejects and their collections.
+class Project
+  attr_reader :funds, :goal, :expire
+  attr_accessor :name
+  def initialize(name="project", funds=0, goal=25, expire=30)
+    @name, @funds, @goal, @expire = name.capitalize, funds, goal, expire
+  end
+  def to_s
+    "#{ @name } has $#{ @funds }, $#{difference} to go, and expires in #{ @expire } days."
+  end
+  def loan
+    @funds += 25
+    puts "Someone loaned $25 to #{@name}!!"
+  end
+  def expirer
+    @expire -= 1
+    puts "#{@name} expires soon, make a loan today!!" if @expire < 5
+  end
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+  def difference
+    @goal - @funds
+  end
+end
