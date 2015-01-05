b19a881618c1e61cbe0d49c72456f082af203c0b
Wilson Cook <wbcook007@gmail.com>
2015-01-05 16:41:42 -0500

    fundraiser.rb comments

--- fundraiser.rb
+++ fundraiser.rb
@@ -1,5 +1,8 @@
-# A fundraising app that tracks the funding of various odd projects. Now with encapsulation
+###
+# A fundraising app that tracks the funding of various odd projects. Now with 
+# encapsulation,
 # and a Fundraising class to manage lists of proejects and their collections.
+###
 require_relative 'project'
 require_relative 'die'
 require_relative 'funding_round'
